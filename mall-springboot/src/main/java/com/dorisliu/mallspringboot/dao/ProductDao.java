d0c3e86506c5c1cfbe105d98cce634159afba79a
Dorisliu18 <sugar840318@gmail.com>
2023-03-28 16:29:09 +0800

    「商品功能 - 實作新增商品」

--- mall-springboot/src/main/java/com/dorisliu/mallspringboot/dao/ProductDao.java
+++ mall-springboot/src/main/java/com/dorisliu/mallspringboot/dao/ProductDao.java
@@ -1,8 +1,11 @@
 package com.dorisliu.mallspringboot.dao;
 
+import com.dorisliu.mallspringboot.dto.ProductRequest;
 import model.Product;
 
 public interface ProductDao {
 
     Product getProductById(Integer productId);
+
+    Integer createProduct(ProductRequest productRequest);
 }
--- mall-springboot/src/main/java/com/dorisliu/mallspringboot/service/ProductService.java
+++ mall-springboot/src/main/java/com/dorisliu/mallspringboot/service/ProductService.java
@@ -1,10 +1,13 @@
 package com.dorisliu.mallspringboot.service;
 
+import com.dorisliu.mallspringboot.dto.ProductRequest;
 import model.Product;
 
 public interface ProductService {
 
     Product getProductById(Integer productId);
+
+    Integer createProduct(ProductRequest productRequest);
 
 
 }
