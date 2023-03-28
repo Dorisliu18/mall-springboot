8d010753cd737b649bd5c2d8b31dcb3071782831
Dorisliu18 <sugar840318@gmail.com>
2023-03-28 16:54:40 +0800

    「商品功能 - 實作修改商品」

--- mall-springboot/src/main/java/com/dorisliu/mallspringboot/controller/ProductController.java
+++ mall-springboot/src/main/java/com/dorisliu/mallspringboot/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.dorisliu.mallspringboot.controller;
 
 import com.dorisliu.mallspringboot.dto.ProductRequest;
 import com.dorisliu.mallspringboot.service.ProductService;
+import jakarta.validation.Valid;
 import model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -27,14 +28,32 @@ public class ProductController {
     }
 
     @PostMapping("/products")
-    public ResponseEntity<Product> createProduct(@RequestBody @Validated ProductRequest productRequest) {
+    public ResponseEntity<Product> createProduct(@RequestBody @Valid ProductRequest productRequest) {
         Integer productId = productService.createProduct(productRequest);
 
         Product product = productService.getProductById(productId);
-
         return  ResponseEntity.status(HttpStatus.CREATED).body(product);
-
     }
+
+    @PutMapping("/products/{productId}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Integer productId,
+                                                 @RequestBody @Valid ProductRequest productRequest){
+
+        // 檢查 product 是否存在
+        Product product = productService.getProductById(productId);
+
+        if (product == null){
+            return  ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        // 修改商品的數據
+        productService.updateProduct(productId, productRequest);
+
+        Product updateProduct = productService.getProductById(productId);
+
+        return ResponseEntity.status(HttpStatus.OK).body(updateProduct);
+    }
+
 
 
 }
